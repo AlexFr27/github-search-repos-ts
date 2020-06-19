3796f3bfb3ae1d0a40ac380a299a155aa3dce658
AlexFr27 <aleksanderstar4encko@yandex.ru>
2020-06-19 13:26:54 +0300

    Updates

---
+++ precache-manifest.5ad5dd7ddb4a8e86bb796895c1eb479e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434f260db8630526d9f8d3468debc10f",
+    "url": "/github-search-repos-ts/index.html"
+  },
+  {
+    "revision": "7e5caa552f06a82c2843",
+    "url": "/github-search-repos-ts/static/css/main.2fc1fe09.chunk.css"
+  },
+  {
+    "revision": "a059d12482f7bdaceed3",
+    "url": "/github-search-repos-ts/static/js/2.efd93d95.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/github-search-repos-ts/static/js/2.efd93d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5caa552f06a82c2843",
+    "url": "/github-search-repos-ts/static/js/main.b10e3a2d.chunk.js"
+  },
+  {
+    "revision": "f1d362847a96ebfe6ef6",
+    "url": "/github-search-repos-ts/static/js/runtime-main.f14fd8b9.js"
+  }
+]);
